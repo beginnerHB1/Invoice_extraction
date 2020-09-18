732abc92639b342de7ab7305683434dfa63ed12b
beginnerHB1 <57528113+beginnerHB1@users.noreply.github.com>
2020-09-18 12:13:51 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 mkdir -p ~/.streamlit/
 echo "\
-[general]\n\
-email = \"bathia.harsh007@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+\n\
+" > ~/.streamlit/config.toml 
