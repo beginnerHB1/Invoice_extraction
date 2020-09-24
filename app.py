7a9ee223551231801efa6a84a769d0dcec8e1f58
beginnerHB1 <57528113+beginnerHB1@users.noreply.github.com>
2020-09-24 14:52:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,5 +53,39 @@ def upload_file():
     return render_template('home.html')
 
 
+
+@app.route('/australia', methods=['GET', 'POST'])
+def upload_file2():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        # try:
+
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        global filename
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            flash('file {} saved'.format(file.filename))
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            # file1 = open("/home/senseque/Desktop/new/testing/myfile.txt","w")
+            # file1.write(f"{filename}")
+            # file1.close()
+            z = find_details_australia(f"data/{filename}")
+            # print(filename)
+            return z
+
+    return render_template('home.html')
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
