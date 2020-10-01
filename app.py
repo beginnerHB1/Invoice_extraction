6f887a19fc9b430f4404ba0cb07c9ac77e450318
beginnerHB1 <57528113+beginnerHB1@users.noreply.github.com>
2020-10-01 15:00:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,40 @@ def upload_file1(company_name):
                 return json.dumps({"response":"please upload unicareers pdfs"}, ensure_ascii=False)
     return render_template('home.html')
 
-#    return render_template('index.html')
+
+@app.route('/auto', methods=['GET', 'POST'])
+def upload_file2():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        global filename
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            flash('file {} saved'.format(file.filename))
+            file.save(filename)
+
+
+        try:
+
+            response = find_details_australia(filename)
+            os.remove(filename)
+            return json.dumps(response, ensure_ascii=False)
+
+        except:
+
+            try:
+                response = extract_detail(filename)
+                os.remove(filename)
+                return json.dumps(response, ensure_ascii=False)
+
+            except Exception as e:
+                os.remove(filename)
+                return json.dumps({"response":"please upload unicareers/australia pdfs"}, ensure_ascii=False)
 
 if __name__ == '__main__':
 	app.run(threaded=True,debug=True)
